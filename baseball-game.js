32bdf8032f2bc03656913cb16b2ba7fa516e05dd
jsw3227 <chosw3227@naver.com>
2025-09-17 17:02:37 +0900

    게임시작 및 어플리케이션 종료, randomNumbers함수 생성

--- baseball-game.js
+++ baseball-game.js
@@ -5,20 +5,34 @@ const rl = readline.createInterface({
   output: process.stdout,
 })
 
+function randomNumbers(){
+    
+}
+
 function start() {
   // rl.question('게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.')
   console.log('게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.')
 
   rl.on('line', (line) => {
-    const randNum = Math.round(Math.random() * 1000)
-    console.log()
+    if(line==='1'){
+        const randNum = Math.round(Math.random() * 1000)
+        console.log()
+        console.log('컴퓨터가 숫자를 뽑았습니다.')
+        rl.on('line',(input)=>{
+            // const test1 = parseInt(line)
+            for (let index = 0; index < input.length; index++) {
+                // if (test1[0] === )
+                console.log(input[index])
+            }
+        })
+        
 
-    console.log('컴퓨터가 숫자를 뽑았습니다.')
-
-    const test1 = parseInt(line)
-    for (let index = 0; index < test1.length; index++) {
-      // if (test1[0] === )
+        
+    }else if(line==='9'){
+        console.log('애플리케이션이 종료되었습니다.')
+        process.exit()
     }
+    
   })
 }
 
